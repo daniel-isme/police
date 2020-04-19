58dc454aa7b4a21a996ef50150940530ee88a718
Ilya Litvinovich <ltvnvchil@gmail.com>
2020-04-19 20:47:20 +0700

    Правые текстбоксы

--- Police/Form1.Designer.cs
+++ Police/Form1.Designer.cs
@@ -28,7 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CR = new System.Windows.Forms.TextBox();
+            this.PE = new System.Windows.Forms.TextBox();
+            this.PWE = new System.Windows.Forms.TextBox();
+            this.CRL = new System.Windows.Forms.TextBox();
+            this.MORAL = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // CR
+            // 
+            this.CR.Location = new System.Drawing.Point(731, 327);
+            this.CR.Name = "CR";
+            this.CR.Size = new System.Drawing.Size(100, 22);
+            this.CR.TabIndex = 0;
+            // 
+            // PE
+            // 
+            this.PE.Location = new System.Drawing.Point(794, 178);
+            this.PE.Name = "PE";
+            this.PE.Size = new System.Drawing.Size(100, 22);
+            this.PE.TabIndex = 1;
+            // 
+            // PWE
+            // 
+            this.PWE.Location = new System.Drawing.Point(675, 130);
+            this.PWE.Name = "PWE";
+            this.PWE.Size = new System.Drawing.Size(100, 22);
+            this.PWE.TabIndex = 2;
+            // 
+            // CRL
+            // 
+            this.CRL.Location = new System.Drawing.Point(523, 431);
+            this.CRL.Name = "CRL";
+            this.CRL.Size = new System.Drawing.Size(100, 22);
+            this.CRL.TabIndex = 3;
+            // 
+            // MORAL
+            // 
+            this.MORAL.Location = new System.Drawing.Point(548, 155);
+            this.MORAL.Name = "MORAL";
+            this.MORAL.Size = new System.Drawing.Size(100, 22);
+            this.MORAL.TabIndex = 4;
             // 
             // Form1
             // 
@@ -36,13 +76,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Police.Properties.Resources.scheme;
             this.ClientSize = new System.Drawing.Size(906, 479);
+            this.Controls.Add(this.MORAL);
+            this.Controls.Add(this.CRL);
+            this.Controls.Add(this.PWE);
+            this.Controls.Add(this.PE);
+            this.Controls.Add(this.CR);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox CR;
+        private System.Windows.Forms.TextBox PE;
+        private System.Windows.Forms.TextBox PWE;
+        private System.Windows.Forms.TextBox CRL;
+        private System.Windows.Forms.TextBox MORAL;
     }
 }
 
