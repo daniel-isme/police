b5f5c6c09e8b2cd9b7e5901e8936894e0d91896e
Dinozaur_G <karnah@mail.ru>
2020-04-19 21:38:01 +0700

    Initial commit to new branch

--- Police/Form1.Designer.cs
+++ Police/Form1.Designer.cs
@@ -28,140 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PDS = new System.Windows.Forms.TextBox();
-            this.PMEN = new System.Windows.Forms.TextBox();
-            this.SALARY = new System.Windows.Forms.TextBox();
-            this.PB = new System.Windows.Forms.TextBox();
-            this.CRLGOVCARE = new System.Windows.Forms.TextBox();
             this.components = new System.ComponentModel.Container();
-            this.CR = new System.Windows.Forms.TextBox();
-            this.PE = new System.Windows.Forms.TextBox();
-            this.PWE = new System.Windows.Forms.TextBox();
-            this.CRL = new System.Windows.Forms.TextBox();
-            this.MORAL = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.illegalValue = new System.Windows.Forms.NumericUpDown();
+            this.depsValue = new System.Windows.Forms.NumericUpDown();
+            this.crimesValue = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartPolice = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.illegalValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.depsValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crimesValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPolice)).BeginInit();
             this.SuspendLayout();
             // 
-            // PDS
+            // panel1
             // 
-            this.PDS.Location = new System.Drawing.Point(113, 39);
-            this.PDS.Name = "PDS";
-            this.PDS.Size = new System.Drawing.Size(100, 22);
-            this.PDS.TabIndex = 0;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.illegalValue);
+            this.panel1.Controls.Add(this.depsValue);
+            this.panel1.Controls.Add(this.crimesValue);
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(932, 122);
+            this.panel1.TabIndex = 0;
             // 
-            // PMEN
+            // label4
             // 
-            this.PMEN.Location = new System.Drawing.Point(253, 49);
-            this.PMEN.Name = "PMEN";
-            this.PMEN.Size = new System.Drawing.Size(100, 22);
-            this.PMEN.TabIndex = 1;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Нелегальные иммигранты:";
             // 
-            // SALARY
+            // label3
             // 
-            this.SALARY.Location = new System.Drawing.Point(312, 161);
-            this.SALARY.Name = "SALARY";
-            this.SALARY.Size = new System.Drawing.Size(100, 22);
-            this.SALARY.TabIndex = 2;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Кол-во отделений полиции:";
             // 
-            // PB
+            // label2
             // 
-            this.PB.Location = new System.Drawing.Point(85, 298);
-            this.PB.Name = "PB";
-            this.PB.Size = new System.Drawing.Size(100, 22);
-            this.PB.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(281, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во преступлений на данный момент:";
             // 
-            // CRLGOVCARE
+            // label1
             // 
-            this.CRLGOVCARE.Location = new System.Drawing.Point(169, 428);
-            this.CRLGOVCARE.Name = "CRLGOVCARE";
-            this.CRLGOVCARE.Size = new System.Drawing.Size(100, 22);
-            this.CRLGOVCARE.TabIndex = 4;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Начальные значения";
             // 
-            // CR
+            // illegalValue
             // 
-            this.CR.Location = new System.Drawing.Point(731, 327);
-            this.CR.Name = "CR";
-            this.CR.Size = new System.Drawing.Size(100, 22);
-            this.CR.TabIndex = 0;
+            this.illegalValue.Location = new System.Drawing.Point(198, 91);
+            this.illegalValue.Name = "illegalValue";
+            this.illegalValue.Size = new System.Drawing.Size(120, 22);
+            this.illegalValue.TabIndex = 3;
             // 
-            // PE
+            // depsValue
             // 
-            this.PE.Location = new System.Drawing.Point(794, 178);
-            this.PE.Name = "PE";
-            this.PE.Size = new System.Drawing.Size(100, 22);
-            this.PE.TabIndex = 1;
+            this.depsValue.Location = new System.Drawing.Point(198, 63);
+            this.depsValue.Name = "depsValue";
+            this.depsValue.Size = new System.Drawing.Size(120, 22);
+            this.depsValue.TabIndex = 2;
             // 
-            // PWE
+            // crimesValue
             // 
-            this.PWE.Location = new System.Drawing.Point(675, 130);
-            this.PWE.Name = "PWE";
-            this.PWE.Size = new System.Drawing.Size(100, 22);
-            this.PWE.TabIndex = 2;
+            this.crimesValue.Location = new System.Drawing.Point(287, 35);
+            this.crimesValue.Name = "crimesValue";
+            this.crimesValue.Size = new System.Drawing.Size(120, 22);
+            this.crimesValue.TabIndex = 1;
             // 
-            // CRL
+            // buttonStart
             // 
-            this.CRL.Location = new System.Drawing.Point(523, 431);
-            this.CRL.Name = "CRL";
-            this.CRL.Size = new System.Drawing.Size(100, 22);
-            this.CRL.TabIndex = 3;
+            this.buttonStart.Location = new System.Drawing.Point(422, 35);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(507, 78);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Старт!!!";
+            this.buttonStart.UseVisualStyleBackColor = true;
             // 
-            // MORAL
+            // chart1
             // 
-            this.MORAL.Location = new System.Drawing.Point(548, 155);
-            this.MORAL.Name = "MORAL";
-            this.MORAL.Size = new System.Drawing.Size(100, 22);
-            this.MORAL.TabIndex = 4;
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 0;
             // 
-            // button1
+            // chart2
             // 
-            this.button1.Location = new System.Drawing.Point(611, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "StartButton";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.chart2.Location = new System.Drawing.Point(0, 0);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 0;
+            // 
+            // chart3
+            // 
+            this.chart3.Location = new System.Drawing.Point(0, 0);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(300, 300);
+            this.chart3.TabIndex = 0;
+            // 
+            // chart4
+            // 
+            this.chart4.Location = new System.Drawing.Point(0, 0);
+            this.chart4.Name = "chart4";
+            this.chart4.Size = new System.Drawing.Size(300, 300);
+            this.chart4.TabIndex = 0;
+            // 
+            // chartPolice
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartPolice.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartPolice.Legends.Add(legend1);
+            this.chartPolice.Location = new System.Drawing.Point(13, 142);
+            this.chartPolice.Name = "chartPolice";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "f4";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartPolice.Series.Add(series1);
+            this.chartPolice.Size = new System.Drawing.Size(929, 300);
+            this.chartPolice.TabIndex = 1;
+            this.chartPolice.Text = "chart5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Police.Properties.Resources.scheme;
-            this.ClientSize = new System.Drawing.Size(906, 479);
-            this.Controls.Add(this.CRLGOVCARE);
-            this.Controls.Add(this.PB);
-            this.Controls.Add(this.SALARY);
-            this.Controls.Add(this.PMEN);
-            this.Controls.Add(this.PDS);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.MORAL);
-            this.Controls.Add(this.CRL);
-            this.Controls.Add(this.PWE);
-            this.Controls.Add(this.PE);
-            this.Controls.Add(this.CR);
+            this.ClientSize = new System.Drawing.Size(957, 453);
+            this.Controls.Add(this.chartPolice);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.illegalValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.depsValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crimesValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPolice)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox PDS;
-        private System.Windows.Forms.TextBox PMEN;
-        private System.Windows.Forms.TextBox SALARY;
-        private System.Windows.Forms.TextBox PB;
-        private System.Windows.Forms.TextBox CRLGOVCARE;
-
-        private System.Windows.Forms.TextBox CR;
-        private System.Windows.Forms.TextBox PE;
-        private System.Windows.Forms.TextBox PWE;
-        private System.Windows.Forms.TextBox CRL;
-        private System.Windows.Forms.TextBox MORAL;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown illegalValue;
+        private System.Windows.Forms.NumericUpDown depsValue;
+        private System.Windows.Forms.NumericUpDown crimesValue;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPolice;
     }
 }
 
--- Police/Form1.cs
+++ Police/Form1.cs
@@ -16,7 +16,6 @@ namespace Police
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Start();
