79ef58bfde814c527a557e9948685f4dc5ddbb7e
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-04-19 21:00:20 +0700

    text box

--- Police/Form1.Designer.cs
+++ Police/Form1.Designer.cs
@@ -28,7 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PDS = new System.Windows.Forms.TextBox();
+            this.PMEN = new System.Windows.Forms.TextBox();
+            this.SALARY = new System.Windows.Forms.TextBox();
+            this.PB = new System.Windows.Forms.TextBox();
+            this.CRLGOVCARE = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // PDS
+            // 
+            this.PDS.Location = new System.Drawing.Point(113, 39);
+            this.PDS.Name = "PDS";
+            this.PDS.Size = new System.Drawing.Size(100, 22);
+            this.PDS.TabIndex = 0;
+            // 
+            // PMEN
+            // 
+            this.PMEN.Location = new System.Drawing.Point(253, 49);
+            this.PMEN.Name = "PMEN";
+            this.PMEN.Size = new System.Drawing.Size(100, 22);
+            this.PMEN.TabIndex = 1;
+            // 
+            // SALARY
+            // 
+            this.SALARY.Location = new System.Drawing.Point(312, 161);
+            this.SALARY.Name = "SALARY";
+            this.SALARY.Size = new System.Drawing.Size(100, 22);
+            this.SALARY.TabIndex = 2;
+            // 
+            // PB
+            // 
+            this.PB.Location = new System.Drawing.Point(85, 298);
+            this.PB.Name = "PB";
+            this.PB.Size = new System.Drawing.Size(100, 22);
+            this.PB.TabIndex = 3;
+            // 
+            // CRLGOVCARE
+            // 
+            this.CRLGOVCARE.Location = new System.Drawing.Point(169, 428);
+            this.CRLGOVCARE.Name = "CRLGOVCARE";
+            this.CRLGOVCARE.Size = new System.Drawing.Size(100, 22);
+            this.CRLGOVCARE.TabIndex = 4;
             // 
             // Form1
             // 
@@ -36,13 +76,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Police.Properties.Resources.scheme;
             this.ClientSize = new System.Drawing.Size(906, 479);
+            this.Controls.Add(this.CRLGOVCARE);
+            this.Controls.Add(this.PB);
+            this.Controls.Add(this.SALARY);
+            this.Controls.Add(this.PMEN);
+            this.Controls.Add(this.PDS);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox PDS;
+        private System.Windows.Forms.TextBox PMEN;
+        private System.Windows.Forms.TextBox SALARY;
+        private System.Windows.Forms.TextBox PB;
+        private System.Windows.Forms.TextBox CRLGOVCARE;
     }
 }
 
